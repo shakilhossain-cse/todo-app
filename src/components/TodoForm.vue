b8e2e730053ab0ba6cd8c200a571b0f7f2b58a07
shakilhossain <shakilhossainp.cse@gmail.com>
2023-08-16 06:42:05 +0600

    set up tailwind css

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,9 @@
+<template>
+           <div class="mb-4">
+            <h1 class="text-grey-darkest">Todo List</h1>
+            <div class="flex mt-4">
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo">
+                <button class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">Add</button>
+            </div>
+        </div>
+</template>
